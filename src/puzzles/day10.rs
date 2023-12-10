66bfa98fd52ac64ed56d6844583175a534841d6f
Christian Legge <christian@christianlegge.dev>
2023-12-10 02:11:37 -0500

    day 10 part 1 struct

--- src/puzzles/day10.rs
+++ src/puzzles/day10.rs
@@ -21,6 +21,12 @@ struct Coords {
     y: usize,
 }
 
+impl PartialEq for Coords {
+    fn eq(&self, other: &Coords) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
+
 enum Direction {
     Left,
     Right,
@@ -29,56 +35,91 @@ enum Direction {
 }
 impl PipeMap {
     fn walk_path(&self) -> usize {
-        let mut length = 0;
-        let mut next = if self.start.x > 0 && self.tiles[self.start.y][self.start.x - 1] == 'F'
+        let mut length = 1;
+        let (mut next, mut dir) = if self.start.x > 0
+            && self.tiles[self.start.y][self.start.x - 1] == 'F'
             || self.tiles[self.start.y][self.start.x - 1] == '7'
         {
-            Coords {
-                x: self.start.x - 1,
-                y: self.start.y,
-            }
+            (
+                Coords {
+                    x: self.start.x - 1,
+                    y: self.start.y,
+                },
+                Direction::Right,
+            )
         } else if self.start.x < self.width - 1 && self.tiles[self.start.y][self.start.x + 1] == 'F'
             || self.tiles[self.start.y][self.start.x + 1] == 'L'
         {
-            Coords {
-                x: self.start.x + 1,
-                y: self.start.y,
-            }
-        } else if self.start.y > 0 && self.tiles[self.start.y - 1][self.start.x];
+            (
+                Coords {
+                    x: self.start.x + 1,
+                    y: self.start.y,
+                },
+                Direction::Left,
+            )
+        } else if self.start.y > 0 && self.tiles[self.start.y - 1][self.start.x] == 'F'
+            || self.tiles[self.start.y - 1][self.start.x] == '7'
+        {
+            (
+                Coords {
+                    x: self.start.x,
+                    y: self.start.y - 1,
+                },
+                Direction::Down,
+            )
+        } else if self.start.y < self.height - 1
+            && self.tiles[self.start.y + 1][self.start.x] == 'J'
+            || self.tiles[self.start.y + 1][self.start.x] == 'L'
+        {
+            (
+                Coords {
+                    x: self.start.x,
+                    y: self.start.y + 1,
+                },
+                Direction::Up,
+            )
+        } else {
+            panic!("error finding first tile");
+        };
+        while next != self.start {
+            next = self.find_next(&dir, &next);
+            length += 1;
+        }
+        length
     }
 
-    fn find_next(&self, from_dir: Direction, cur_tile: Coords) -> Coords {
+    fn find_next(&self, from_dir: &Direction, cur_tile: &Coords) -> Coords {
         let tile_value = self.tiles[cur_tile.y][cur_tile.x];
         match (from_dir, tile_value) {
-            (Left, '7') => Coords {
+            (Direction::Left, '7') => Coords {
                 x: cur_tile.x,
                 y: cur_tile.y + 1,
             },
-            (Left, 'J') => Coords {
+            (Direction::Left, 'J') => Coords {
                 x: cur_tile.x,
                 y: cur_tile.y - 1,
             },
-            (Right, 'L') => Coords {
+            (Direction::Right, 'L') => Coords {
                 x: cur_tile.x,
                 y: cur_tile.y - 1,
             },
-            (Right, 'F') => Coords {
+            (Direction::Right, 'F') => Coords {
                 x: cur_tile.x,
                 y: cur_tile.y + 1,
             },
-            (Up, 'L') => Coords {
+            (Direction::Up, 'L') => Coords {
                 x: cur_tile.x + 1,
                 y: cur_tile.y,
             },
-            (Up, 'J') => Coords {
+            (Direction::Up, 'J') => Coords {
                 x: cur_tile.x - 1,
                 y: cur_tile.y,
             },
-            (Down, '7') => Coords {
+            (Direction::Down, '7') => Coords {
                 x: cur_tile.x - 1,
                 y: cur_tile.y,
             },
-            (Down, 'F') => Coords {
+            (Direction::Down, 'F') => Coords {
                 x: cur_tile.x + 1,
                 y: cur_tile.y,
             },
