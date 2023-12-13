b403af8ae804a9e63b8bc5afa88bf9cf3236daa2
Christian Legge <christian@christianlegge.dev>
2023-12-13 09:54:14 -0500

    day 13 part 2

--- src/puzzles/day13.rs
+++ src/puzzles/day13.rs
@@ -51,10 +51,14 @@ impl Grid {
     }
 
     fn get_mirror_lines(&self) -> MirrorLines {
-        let horizontal_axis =
-            find_mirror_point(self.rows.iter().map(|s| s.as_str()).collect::<Vec<_>>());
-        let vertical_axis =
-            find_mirror_point(self.cols.iter().map(|s| s.as_str()).collect::<Vec<_>>());
+        let horizontal_axis = find_mirror_point(
+            self.rows.iter().map(|s| s.as_str()).collect::<Vec<_>>(),
+            MirrorLines::Horizontal(0),
+        );
+        let vertical_axis = find_mirror_point(
+            self.cols.iter().map(|s| s.as_str()).collect::<Vec<_>>(),
+            MirrorLines::Vertical(0),
+        );
 
         if horizontal_axis > 0 {
             dbg!(MirrorLines::Horizontal(horizontal_axis))
@@ -75,7 +79,12 @@ impl Grid {
                 let mut new_grid = grid.clone();
                 new_grid.replace_range(idx..idx + 1, if char == '#' { "." } else { "#" });
 
-                let new_axis = find_mirror_point(new_grid.split("\n").collect::<Vec<_>>());
+                println!("looking in new grid:\n{}", new_grid);
+                let new_axis = find_mirror_point_old(
+                    new_grid.split("\n").collect::<Vec<_>>(),
+                    original,
+                    MirrorLines::Horizontal(0),
+                );
                 if new_axis > 0 && MirrorLines::Horizontal(new_axis) != original {
                     return MirrorLines::Horizontal(new_axis);
                 }
@@ -88,8 +97,15 @@ impl Grid {
             if char == '#' || char == '.' {
                 let mut new_grid = grid.clone();
                 new_grid.replace_range(idx..idx + 1, if char == '#' { "." } else { "#" });
-
-                let new_axis = find_mirror_point(new_grid.split("\n").collect::<Vec<_>>());
+                // let mut to_print = new_grid.clone();
+                // to_print.replace_range(idx..idx + 1, "[X]");
+                //
+                // println!("looking in new grid:\n{}", to_print);
+                let new_axis = find_mirror_point_old(
+                    new_grid.split("\n").collect::<Vec<_>>(),
+                    original,
+                    MirrorLines::Vertical(0),
+                );
                 if new_axis > 0 && MirrorLines::Vertical(new_axis) != original {
                     return MirrorLines::Vertical(new_axis);
                 }
@@ -99,38 +115,47 @@ impl Grid {
         panic!("smudge line not found");
     }
 }
+fn find_mirror_point(lines: Vec<&str>, this_axis: MirrorLines) -> usize {
+    find_mirror_point_old(lines, MirrorLines::Horizontal(0), this_axis)
+}
 
-fn find_mirror_point(lines: Vec<&str>) -> usize {
+fn find_mirror_point_old(lines: Vec<&str>, old_axis: MirrorLines, this_axis: MirrorLines) -> usize {
     let mut axis = 1;
     while axis < lines.len() {
         let mut mirrored_lines = 0;
         let (mut left, mut right): (i32, usize) =
             ((axis - 1 - mirrored_lines) as i32, axis + mirrored_lines);
         while left >= 0 && right < lines.len() {
-            println!(
-                "checking equality:\n[{}] {}\n[{}] {}",
-                left, lines[left as usize], right, lines[right]
-            );
+            // println!(
+            //     "checking equality:\n[{}] {}\n[{}] {}",
+            //     left, lines[left as usize], right, lines[right]
+            // );
             if lines[left as usize] == lines[right] {
-                println!("true");
+                // println!("true");
                 mirrored_lines += 2;
                 left -= 1;
                 right += 1;
             } else {
-                println!("false");
+                // println!("false");
                 break;
             }
         }
         if !(left >= 0 && right < lines.len()) {
             println!("mirror axis found: {}", axis);
-            return axis;
+            let current_axis = match this_axis {
+                MirrorLines::Horizontal(_) => MirrorLines::Horizontal(axis),
+                MirrorLines::Vertical(_) => MirrorLines::Vertical(axis),
+            };
+            if current_axis != old_axis {
+                return axis;
+            }
         }
         axis += 1;
     }
     0
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 enum MirrorLines {
     Horizontal(usize),
     Vertical(usize),
