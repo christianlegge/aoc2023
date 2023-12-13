c8740e901e02d26820f1a380b7bbeaae3c73d149
Christian Legge <christian@christianlegge.dev>
2023-12-12 22:45:23 -0500

    day 12 part 1

--- src/puzzles/day12.rs
+++ src/puzzles/day12.rs
@@ -11,6 +11,10 @@ fn test() {
 ?###???????? 3,2,1",
     ));
 }
+#[test]
+fn test2() {
+    dbg!(count_valid_configurations("?###???????? 3,2,1"));
+}
 
 #[derive(Debug)]
 enum SpringRun {
@@ -22,6 +26,7 @@ enum SpringRun {
 #[derive(Debug)]
 struct SpringRow {
     spring_runs: Vec<SpringRun>,
+    springs: Vec<char>,
     groups: Vec<u32>,
 }
 
@@ -30,6 +35,8 @@ impl SpringRow {
         let mut parts = data.split_whitespace();
         let springs = parts.next().unwrap();
         let groups = parts.next().unwrap();
+
+        let mut spring_chars = Vec::new();
 
         let mut chars = springs.chars();
         let mut curr = chars.next().unwrap();
@@ -47,6 +54,7 @@ impl SpringRow {
                 });
                 num = 1;
             }
+            spring_chars.push(char);
             curr = char;
         }
         runs.push(match curr {
@@ -58,18 +66,91 @@ impl SpringRow {
 
         SpringRow {
             spring_runs: runs,
+            springs: spring_chars,
             groups: groups
                 .split(",")
                 .map(|s| s.parse().unwrap())
                 .collect::<Vec<u32>>(),
         }
     }
+
+    fn is_valid(&self) -> Option<bool> {
+        let damaged_runs = self
+            .spring_runs
+            .iter()
+            .filter(|c| matches!(c, SpringRun::Damaged(_)))
+            .map(|c| {
+                if let SpringRun::Damaged(n) = c {
+                    n
+                } else {
+                    panic!("error in is_valid finding damaged_runs")
+                }
+            });
+        if damaged_runs.clone().count() == self.groups.len()
+            && damaged_runs.zip(&self.groups).all(|(a, b)| a == b)
+        {
+            Some(true)
+        } else {
+            if self
+                .spring_runs
+                .iter()
+                .any(|c| matches!(c, SpringRun::Unknown(_)))
+            {
+                None
+            } else {
+                Some(false)
+            }
+        }
+    }
+}
+
+fn count_valid_configurations(data: &str) -> usize {
+    // println!("counting valid configurations for {}", data);
+    let row = SpringRow::new(data);
+    match row.is_valid() {
+        Some(valid) => {
+            if valid {
+                1
+            } else {
+                0
+            }
+        }
+        None => {
+            let parts = data.splitn(2, "?");
+            // let next_row = remove_first_question(data);
+            count_valid_configurations(&format!(
+                "{}.{}",
+                parts.clone().next().unwrap(),
+                parts.clone().nth(1).unwrap()
+            )) + count_valid_configurations(&format!(
+                "{}#{}",
+                parts.clone().next().unwrap(),
+                parts.clone().nth(1).unwrap()
+            ))
+        }
+    }
+}
+
+fn remove_first_question(data: &str) -> String {
+    data.chars()
+        .skip_while(|c| c != &'?')
+        .skip(1)
+        .collect::<String>()
 }
 
 pub fn solve(data: String) {
     println!("{}", data);
+    let mut sum = 0;
     for line in data.lines() {
         let row = SpringRow::new(line);
-        dbg!(row);
+        let mut parts = line.split_whitespace();
+        // let springs = parts.next().unwrap().repeat(5);
+        // let groups = parts.next().unwrap().repeat(5);
+        // let configs = count_valid_configurations(&format!("{} {}", springs, groups));
+        let configs = count_valid_configurations(line);
+        // dbg!(row);
+        println!("valid configurations for {}: {}", line, configs);
+        sum += configs;
     }
+    println!("total configs: {}", sum);
 }
