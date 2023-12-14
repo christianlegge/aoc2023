53edef0d4d38fa02f14b6b982334144a3c8aed10
Christian Legge <christian@christianlegge.dev>
2023-12-14 17:09:42 -0500

    day 14 part 2 (1+ minute)

--- src/puzzles/day14.rs
+++ src/puzzles/day14.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[test]
 fn test() {
     solve(String::from(
@@ -14,11 +16,20 @@ O.#..O.#.#
     ));
 }
 
+enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
+
 struct RockGrid {
     walls: Vec<(usize, usize)>,
     width: usize,
     height: usize,
     rocks: Vec<(usize, usize)>,
+    cycle_states: HashMap<String, usize>,
+    cycle_loads: HashMap<usize, usize>,
 }
 
 impl RockGrid {
@@ -41,30 +52,71 @@ impl RockGrid {
             width,
             height,
             rocks,
+            cycle_states: HashMap::new(),
+            cycle_loads: HashMap::new(),
         }
     }
 
-    fn sort_rocks(&mut self) {
-        self.rocks
-            .sort_by(|(row, _col), (row2, _col2)| row2.partial_cmp(row).unwrap());
+    fn sort_rocks(&mut self, dir: Direction) {
+        match dir {
+            Direction::North => self
+                .rocks
+                .sort_by(|(row, _col), (row2, _col2)| row2.partial_cmp(row).unwrap()),
+            Direction::East => self
+                .rocks
+                .sort_by(|(_row, col), (_row2, col2)| col.partial_cmp(col2).unwrap()),
+            Direction::South => self
+                .rocks
+                .sort_by(|(row, _col), (row2, _col2)| row.partial_cmp(row2).unwrap()),
+            Direction::West => self
+                .rocks
+                .sort_by(|(_row, col), (_row2, col2)| col2.partial_cmp(col).unwrap()),
+        }
     }
 
-    fn shift_north(&mut self) {
+    fn shift(&mut self, dir: Direction) {
         let mut new_rocks = Vec::new();
+        let bound = match dir {
+            Direction::North => -1,
+            Direction::East => self.width as i32,
+            Direction::South => self.height as i32,
+            Direction::West => -1,
+        };
+        let step = match dir {
+            Direction::North => -1,
+            Direction::East => 1,
+            Direction::South => 1,
+            Direction::West => -1,
+        };
         while let Some((row, col)) = self.rocks.pop() {
-            let mut wall_row = row as i32;
-            let new_rock = (
-                loop {
-                    wall_row -= 1;
-                    if wall_row == -1
-                        || self.walls.contains(&(wall_row as usize, col))
-                        || new_rocks.contains(&(wall_row as usize, col))
-                    {
-                        break (wall_row + 1) as usize;
-                    }
-                },
-                col,
-            );
+            let mut wall_coord = match dir {
+                Direction::North => row,
+                Direction::East => col,
+                Direction::South => row,
+                Direction::West => col,
+            } as i32;
+            let final_coord = loop {
+                wall_coord += step;
+                let new_coord = match dir {
+                    Direction::North => (wall_coord as usize, col),
+                    Direction::East => (row, wall_coord as usize),
+                    Direction::South => (wall_coord as usize, col),
+                    Direction::West => (row, wall_coord as usize),
+                };
+                if wall_coord == bound
+                    || self.walls.contains(&new_coord)
+                    || new_rocks.contains(&new_coord)
+                {
+                    break (wall_coord - step) as usize;
+                }
+            };
+            let new_rock = match dir {
+                Direction::North => (final_coord, col),
+                Direction::East => (row, final_coord),
+                Direction::South => (final_coord, col),
+                Direction::West => (row, final_coord),
+            };
+
             // println!("rock at {}, {} moved to row {}", row, col, new_rock.0);
             new_rocks.push(new_rock);
         }
@@ -80,7 +132,7 @@ impl RockGrid {
         sum
     }
 
-    fn print_grid(&self) {
+    fn to_string(&self) -> String {
         let mut grid = String::new();
         for row in 0..self.height {
             for col in 0..self.width {
@@ -98,22 +150,56 @@ impl RockGrid {
             }
             grid.push('\n');
         }
-        println!("grid:\n{}\n", grid);
+        grid
+    }
+
+    fn print_grid(&self) {
+        println!("grid:\n{}\n", self.to_string());
+    }
+
+    fn cycle(&mut self) {
+        self.sort_rocks(Direction::North);
+        self.shift(Direction::North);
+        self.sort_rocks(Direction::West);
+        self.shift(Direction::West);
+        self.sort_rocks(Direction::South);
+        self.shift(Direction::South);
+        self.sort_rocks(Direction::East);
+        self.shift(Direction::East);
+    }
+
+    fn check_for_cycle(&mut self, iter: usize) {
+        let grid = self.to_string();
+        if let Some(cycle_start) = self.cycle_states.get(&grid) {
+            let period = iter - cycle_start;
+            let remainder = (1000000000 - cycle_start) % period;
+            panic!(
+                "cycle found at {}..{} (final load: {})",
+                cycle_start,
+                iter,
+                self.cycle_loads.get(&(remainder + cycle_start)).unwrap()
+            );
+        } else {
+            self.cycle_states.insert(grid, iter);
+            self.cycle_loads.insert(iter, self.calculate_load());
+        }
     }
 }
 
 pub fn solve(data: String) {
     // println!("{}", data);
     let mut grid = RockGrid::new(data);
-    grid.print_grid();
-    grid.sort_rocks();
-    // grid.shift_north();
+    // grid.print_grid();
+    // grid.sort_rocks(Direction::North);
+    // grid.shift(Direction::North);
     // grid.print_grid();
     for i in 0..1000000000 {
-        // if i % 10000 == 0 {
         println!("iteration {}", i);
+        grid.check_for_cycle(i);
+        grid.cycle();
+        // if i % 10000 == 0 {
         // }
-        grid.shift_north();
+        // grid.shift(Direction::North);
     }
 
     dbg!(grid.calculate_load());
