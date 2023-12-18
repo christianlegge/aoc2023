bbca3302b60c7f1d431f03a8372c192acd51b158
Christian Legge <christian@christianlegge.dev>
2023-12-17 19:54:58 -0500

    day 17 part 1

--- src/puzzles/day17.rs
+++ src/puzzles/day17.rs
@@ -19,7 +19,7 @@ fn test() {
     ));
 }
 
-#[derive(Eq, PartialEq, Debug, Hash)]
+#[derive(Eq, Copy, Clone, PartialEq, Debug, Hash)]
 enum Direction {
     Up(usize),
     Down(usize),
@@ -40,7 +40,7 @@ struct CrucibleMap {
     height: usize,
 }
 
-#[derive(Hash, Eq, PartialEq, Debug)]
+#[derive(Hash, Eq, PartialEq, Debug, Copy, Clone)]
 struct AStarScores {
     parent: Coords,
     dir: Direction,
@@ -64,68 +64,187 @@ impl CrucibleMap {
         }
     }
 
-    fn find_path(&self) -> usize {
+    fn draw_path(&self, path: &Vec<Coords>) -> usize {
+        let mut sum = 0;
+        for i in 0..self.height {
+            for j in 0..self.width {
+                // if let Some(el) = path.get(&Coords { row: i, col: j }) {
+                //     print!(
+                //         "{}",
+                //         match el.dir {
+                //             Direction::Up(_) => '^',
+                //             Direction::Down(_) => 'v',
+                //             Direction::Left(_) => '<',
+                //             Direction::Right(_) => '>',
+                //         }
+                //     );
+                if path.contains(&Coords { row: i, col: j }) {
+                    print!("#");
+                    sum += self.get_cell_value(&Coords { row: i, col: j });
+                } else {
+                    print!("{}", self.grid[i][j]);
+                }
+            }
+            print!("\n");
+        }
+        sum
+    }
+
+    fn get_cell_value(&self, coords: &Coords) -> usize {
+        self.grid[coords.row][coords.col].to_digit(10).unwrap() as usize
+    }
+
+    fn find_lowest_f(
+        &self,
+        list: &HashMap<(Coords, Direction), AStarScores>,
+    ) -> ((Coords, Direction), AStarScores) {
+        let mut kvs = list.iter().collect::<Vec<_>>();
+        kvs.sort_by(|(k1, v1), (k2, v2)| (v1.g + v1.h).partial_cmp(&(v2.g + v2.h)).unwrap());
+        let curr = kvs.first().unwrap();
+        (*(*curr).0, *(*curr).1)
+    }
+
+    fn find_path(&self) -> Vec<Coords> {
         let mut open = HashMap::new();
         let mut closed = HashMap::new();
-        let mut curr = Coords { row: 0, col: 0 };
-        let mut curr_scores = AStarScores {
-            g: 0,
-            h: self.heuristic(curr),
-            dir: Direction::Right(0),
-            parent: curr,
-        };
-        open.insert(curr, curr_scores);
-        loop {
+        open.insert(
+            (Coords { row: 0, col: 0 }, Direction::Right(0)),
+            AStarScores {
+                g: 0,
+                h: self.heuristic(Coords { row: 0, col: 0 }),
+                dir: Direction::Right(0),
+                parent: Coords { row: 0, col: 0 },
+            },
+        );
+        let path_len = loop {
+            println!("open: {}, closed: {}", open.len(), closed.len());
             if open.len() == 0 {
                 panic!("unable to find path");
             }
-            let mut kvs = open.iter().collect::<Vec<_>>();
-            kvs.sort_by(|(k1, v1), (k2, v2)| )
-            let valid_next = self.find_valid_next(curr, curr_scores.dir);
-        }
-        0
+            // let mut curr = open.clone().iter().next().unwrap().clone();
+            // let mut min = curr.1.g + curr.1.h;
+            // for sq in &open {
+            //     if sq.1.g + sq.1.h < min {
+            //         min = sq.1.g + sq.1.h;
+            //         curr = sq.clone();
+            //     }
+            // }
+            let curr = self.find_lowest_f(&open);
+            // dbg!(curr);
+            // let mut kvs = open.iter().collect::<Vec<_>>();
+            // kvs.sort_by(|(k1, v1), (k2, v2)| (v1.g + v1.h).partial_cmp(&(v2.g + v2.h)).unwrap());
+            // let curr = kvs.first().unwrap();
+            open.remove(&curr.0);
+            closed.insert(curr.0, curr.1);
+            if curr.0 .0.row == self.height - 1 && curr.0 .0.col == self.width - 1 {
+                break curr.1.g;
+            }
+            let valid_next = self.find_valid_next(curr.0 .0, curr.1.dir);
+            // dbg!(&valid_next);
+            for (next, dir) in valid_next {
+                if closed.contains_key(&(next, dir)) {
+                    continue;
+                } else if let Some(scores) = open.get_mut(&(next, dir)) {
+                    if curr.1.g + self.get_cell_value(&next) < scores.g {
+                        scores.g = curr.1.g + self.get_cell_value(&next);
+                        scores.parent = curr.0 .0;
+                        scores.dir = dir;
+                    }
+                } else {
+                    open.insert(
+                        (next, dir),
+                        AStarScores {
+                            parent: curr.0 .0,
+                            dir,
+                            g: curr.1.g + self.get_cell_value(&next),
+                            h: self.heuristic(next),
+                        },
+                    );
+                }
+            }
+        };
+        let mut curr = Coords {
+            row: self.height - 1,
+            col: self.width - 1,
+        };
+        let mut path = vec![curr];
+        // while curr != (Coords { row: 0, col: 0 }) {
+        //     curr = closed.get(&curr).unwrap().parent;
+        //     path.push(curr);
+        // }
+        dbg!(path_len);
+        path
     }
 
-    fn find_valid_next(&self, curr: Coords, dir: Direction) -> Vec<Coords> {
+    fn find_valid_next(&self, curr: Coords, dir: Direction) -> Vec<(Coords, Direction)> {
         let mut coords = Vec::new();
         if curr.row > 0 && dir != Direction::Up(3) && !matches!(dir, Direction::Down(_)) {
-            coords.push(Coords {
-                row: curr.row - 1,
-                col: curr.col,
-            })
+            coords.push((
+                Coords {
+                    row: curr.row - 1,
+                    col: curr.col,
+                },
+                if let Direction::Up(n) = dir {
+                    Direction::Up(n + 1)
+                } else {
+                    Direction::Up(1)
+                },
+            ))
         }
         if curr.row < self.height - 1
             && dir != Direction::Down(3)
             && !matches!(dir, Direction::Up(_))
         {
-            coords.push(Coords {
-                row: curr.row + 1,
-                col: curr.col,
-            })
+            coords.push((
+                Coords {
+                    row: curr.row + 1,
+                    col: curr.col,
+                },
+                if let Direction::Down(n) = dir {
+                    Direction::Down(n + 1)
+                } else {
+                    Direction::Down(1)
+                },
+            ))
         }
         if curr.col > 0 && dir != Direction::Left(3) && !matches!(dir, Direction::Right(_)) {
-            coords.push(Coords {
-                row: curr.row,
-                col: curr.col - 1,
-            })
+            coords.push((
+                Coords {
+                    row: curr.row,
+                    col: curr.col - 1,
+                },
+                if let Direction::Left(n) = dir {
+                    Direction::Left(n + 1)
+                } else {
+                    Direction::Left(1)
+                },
+            ))
         }
         if curr.col < self.width - 1
             && dir != Direction::Right(3)
             && !matches!(dir, Direction::Left(_))
         {
-            coords.push(Coords {
-                row: curr.row,
-                col: curr.col + 1,
-            })
+            coords.push((
+                Coords {
+                    row: curr.row,
+                    col: curr.col + 1,
+                },
+                if let Direction::Right(n) = dir {
+                    Direction::Right(n + 1)
+                } else {
+                    Direction::Right(1)
+                },
+            ))
         }
         coords
     }
 
     fn heuristic(&self, coords: Coords) -> usize {
-        (self.height - coords.row) + (self.width - coords.col)
+        (self.height - coords.row) + (self.width - coords.col) - 2
     }
 }
 pub fn solve(data: String) {
     let grid = CrucibleMap::new(data);
-    dbg!(grid.find_valid_next(Coords { row: 0, col: 0 }, Direction::Right(0)));
+    let path = grid.find_path();
+    // dbg!(grid.draw_path(&path));
 }
